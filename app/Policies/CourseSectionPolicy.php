16e4fdc667208cedf070c4109120ee3d9d8b2ca9
Kipserem L <161978150+Achegei@users.noreply.github.com>
2025-08-28 09:38:32 +0300

    access to courses

--- app/Policies/CourseSectionPolicy.php
+++ app/Policies/CourseSectionPolicy.php
@@ -2,49 +2,36 @@
 
 namespace App\Policies;
 
-use App\Models\Blog;
 use App\Models\User;
+use App\Models\CourseSection;
 use Illuminate\Auth\Access\Response;
 
-class BlogPolicy
+class CourseSectionPolicy
 {
     /**
      * Determine whether the user can view any models.
+     * This method could be used to control if a user can even see the list of citizenship courses.
      */
     public function viewAny(User $user): bool
     {
-        return $user->role === 1;
+        // For now, allow any logged-in user to see the list of citizenship courses.
+        // Access to individual sections will be controlled by 'view'.
+        return $user !== null; // User must be logged in to see any course list
     }
 
     /**
-     * Determine whether the user can view the model.
+     * Determine whether the user can view the CourseSection.
      */
-    public function view(User $user, Blog $blog): bool
+    public function view(User $user, CourseSection $courseSection): bool
     {
-        return $user->role === 1;
+        // Admins can view any course section
+        if ($user->isAdmin()) {
+            return true;
+        }
+
+        // Check if the user has access to this specific citizenship course section
+        return $user->citizenshipSections->contains($courseSection->id);
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return $user->role === 1;
-    }
-
-    /**
-     * Determine whether the user can update the model.
-     */
-    public function update(User $user, Blog $blog): bool
-    {
-        return $user->role === 1;
-    }
-
-    /**
-     * Determine whether the user can delete the model.
-     */
-    public function delete(User $user, Blog $blog): bool
-    {
-        return $user->role === 1;
-    }
+    // You might also define other methods like create, update, delete if applicable
 }
